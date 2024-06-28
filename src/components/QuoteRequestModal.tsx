a9073a3a8a35255183f3b1421fefb143f5c40bc7
Aldren Kent  Cirunay <131524402+kynttt@users.noreply.github.com>
2024-06-27 22:41:42 -0700

    added quoteRequestModal

---
+++ src/components/QuoteRequestModal.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import OTPImage from '../assets/img/quoteRequest.png';
+import Button from './Button';
+
+const QuoteRequestModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
+  const [seconds, setSeconds] = useState(15);
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>;
+    if (isOpen) {
+      timer = setInterval(() => {
+        setSeconds(prevSeconds => (prevSeconds > 0 ? prevSeconds - 1 : 0));
+      }, 1000);
+    }
+    return () => {
+      clearInterval(timer);
+      setSeconds(15);
+    };
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-10 backdrop-filter backdrop-blur-sm">
+      <div className="relative w-full max-w-md p-6 sm:p-8 md:p-10 bg-white rounded-lg">
+        <button
+          className="absolute top-2 right-2 text-gray-500 px-2"
+          onClick={onClose}
+        >
+          &times;
+        </button>
+        <div className="text-center">
+          <img src={OTPImage} alt="Verification" className="mx-auto mb-4 w-32 sm:w-48" />
+          <h2 className="mb-2 text-xl font-semibold text-secondary">Booking Request Sent</h2>
+          <p className="mb-6 font-normal text-sm text-primary">
+          Wait for the dispatcher to confirm your quote and booking.
+          </p>
+          
+          <div className="flex justify-center">
+            <Button
+              label="OK"
+              size="medium"
+              bgColor="#252F70"
+              fontStyle="thin"
+              onClick={() => console.log('Button Clicked')}
+              className="extra-class-for-medium-button"
+            />
+          </div>
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
+};
+
+export default QuoteRequestModal;
