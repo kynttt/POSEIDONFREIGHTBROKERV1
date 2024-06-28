e364a72c3211d566a184f091db174134957b14e7
Aldren Kent  Cirunay <131524402+kynttt@users.noreply.github.com>
2024-06-27 19:08:04 -0700

    change faqs bg

--- src/components/Faqs.tsx
+++ src/components/Faqs.tsx
@@ -21,14 +21,14 @@ const FAQsPage: React.FC = () => {
   ];
 
   return (
-    <div className=" bg-light-grey flex flex-col items-center mt-20">
+    <div className=" bg-white flex flex-col items-center mt-20">
       <div className="w-full max-w-5xl p-8">
         <h1 className="text-4xl font-medium text-primary my-10">FAQs</h1>
         {faqs.map((faq, index) => (
           <div key={index} className="mb-8">
             <h2 className="text-xl font-medium text-secondary mb-2">{faq.question}</h2>
             <p className="text-gray-500 text-sm font-normal mb-10 text-justify">{faq.answer}</p>
-            {index < faqs.length + 1 && <hr className="mt-6 border-white" />}
+            {index < faqs.length + 1 && <hr className="mt-6 border-light-grey" />}
           </div>
         ))}
       </div>
