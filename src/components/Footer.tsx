3595cf25e390d4a153f54ca8bc14f27633cea148
Aldren Kent  Cirunay <131524402+kynttt@users.noreply.github.com>
2024-06-27 18:12:39 -0700

    revised footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,15 @@ import React from 'react';
 const Footer: React.FC = () => {
     return (
         <footer className="bg-secondary text-white py-8">
-            <div className="container mx-auto px-4 md:px-8 lg:px-16 grid grid-cols-1 lg:grid-cols-4 gap-8">
+            <div className="container mx-auto px-4 md:px-8 lg:px-14 grid grid-cols-1 lg:grid-cols-5 gap-8">
                 <div>
                     <h2 className="font-medium mb-4 text-2xl">Freight Brokerage</h2>
+                    <h2 className="font-medium  text-2xl">Transport</h2>
+                    <h2 className="font-medium  text-2xl text-primary">Logistics</h2>
+                    
+                </div>
+                <div className='lg:ml-16'>
+                    <h2 className="font-medium mb-4 text-2xl">Trailers</h2>
                     <ul>
                         <li className='font-light'>Full Truckload</li>
                         <li className='font-light'>Flat Bed</li>
@@ -13,7 +19,7 @@ const Footer: React.FC = () => {
                         <li className='font-light'>Dry Van</li>
                     </ul>
                 </div>
-                <div className='lg:ml-40'>
+                <div className='lg:ml-10'>
                     <h2 className="font-medium mb-4 text-2xl">Services</h2>
                     <ul>
                         <li className='font-light'>Carriers</li>
@@ -21,7 +27,7 @@ const Footer: React.FC = () => {
                         <li className='font-light'>Shippers</li>
                     </ul>
                 </div>
-                <div className='lg:ml-24'>
+                <div >
                     <h2 className="font-medium mb-4 text-2xl">Quick Links</h2>
                     <ul>
                         <li className='font-light'>About Us</li>
@@ -34,7 +40,7 @@ const Footer: React.FC = () => {
                     <h2 className="font-medium mb-4 text-2xl">Connect with us</h2>
                     <address>
                         <p className='font-light'>Freight Brokerage</p>
-                        <p className='font-light'>1020 A St SE Suit 7 Auburn WA 98002</p>
+                        <p className='font-light mb-4'>1020 A St SE Suit 7 Auburn WA 98002</p>
                         <p className='font-light'>Email: <a href="mailto:info@pdienterprise.com" className="text-white underline font-light">info@pdienterprise.com</a></p>
                         <p className='font-light'>PHONE: <a href="tel:+12532691300" className="text-white underline font-light">(253) 269 1300</a></p>
                         <p className='font-light'>FAX: <a href="tel:+12532895660" className="text-white underline font-light">(253) 289 5660</a></p>
