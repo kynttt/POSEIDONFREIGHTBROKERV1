c3b94e8f477d683a74115272e6fe1fa43bbe06a5
Aldren Kent  Cirunay <131524402+kynttt@users.noreply.github.com>
2024-06-30 23:29:32 -0700

    loadboardCard made responsive

--- src/components/LoadCard.tsx
+++ src/components/LoadCard.tsx
@@ -25,73 +25,71 @@ const LoadCard: React.FC<CardProps> = ({
     return (
         <div className="bg-light-grey text-primary shadow-xl rounded-lg p-8 mb-4 text-xs my-8">
             <div className="grid grid-cols-8 gap-4">
-                <div className="flex items-center flex-col">
+                <div className="flex items-center flex-col col-span-2 md:col-span-1">
                     <div className="flex items-center">
                         <p className="font-bold">Post ID:</p>
                     </div>
                     <div>
-                        <p className=" font-normal">{id}</p>
+                        <p className="font-normal">{id}</p>
                     </div>
                 </div>
-                <div className="flex items-center flex-col">
+                <div className="flex items-center flex-col col-span-2 md:col-span-1">
                     <div className="flex items-center">
                         <p className="font-bold">Pick</p>
                     </div>
                     <div>
-                        <p className=" font-normal">{pickUp}</p>
+                        <p className="font-normal">{pickUp}</p>
                     </div>
                 </div>
-                <div className="flex items-center flex-col">
+                <div className="flex items-center flex-col col-span-2 md:col-span-1">
                     <div className="flex items-center">
                         <p className="font-bold">Drop</p>
                     </div>
                     <div>
-                        <p className=" font-normal">{drop}</p>
+                        <p className="font-normal">{drop}</p>
                     </div>
                 </div>
-                <div className="flex items-center flex-col">
+                <div className="flex items-center flex-col col-span-2 md:col-span-1">
                     <div className="flex items-center">
                         <p className="font-bold">Company Name</p>
                     </div>
                     <div>
-                        <p className=" font-normal">{companyName}</p>
+                        <p className="font-normal">{companyName}</p>
                     </div>
                 </div>
-                <div className="flex items-center flex-col">
+                <div className="flex items-center flex-col col-span-2 md:col-span-1">
                     <div className="flex items-center">
                         <p className="font-bold">Trailer Type</p>
                     </div>
                     <div>
-                        <p className=" font-normal">{trailerType}</p>
+                        <p className="font-normal">{trailerType}</p>
                     </div>
                 </div>
-                <div className="flex items-center flex-col">
+                <div className="flex items-center flex-col col-span-2 md:col-span-1">
                     <div className="flex items-center">
                         <p className="font-bold">Trailer Size</p>
                     </div>
                     <div>
-                        <p className=" font-normal">{trailerSize}</p>
+                        <p className="font-normal">{trailerSize}</p>
                     </div>
                 </div>
-                <div className="flex items-center flex-col">
+                <div className="flex items-center flex-col col-span-2 md:col-span-1">
                     <div className="flex items-center">
                         <p className="font-bold">Load Price</p>
                     </div>
                     <div>
-                        <p className=" text-xl font-medium text-green">${loadPrice}</p>
+                        <p className="text-xl font-medium text-green">${loadPrice}</p>
                     </div>
                 </div>
-
-                
-                <div className="flex items-center">
-                <Button
-                                    label="Book Load"
-                                    size="medium"
-                                    bgColor="#252F70"
-                                    hoverBgColor="white"
-                                    onClick={{}={}}
-                                    className="extra-class-for-medium-button"
-                                />
+                <div className="flex items-center col-span-2 md:col-span-1">
+                    <Button
+                        label="Book Load"
+                        size="medium"
+                        bgColor="#252F70"
+                        hoverBgColor="white"
+                        onClick={onBookLoadClick}
+                        className="extra-class-for-medium-button"
+                    />
                 </div>
             </div>
         </div>
