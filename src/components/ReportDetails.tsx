3d8307c00b89d3714469098fa3dcaeaa3b913ae5
KennethKnows <abenojaken@gmail.com>
2024-07-02 16:46:19 +0800

    update 1

---
+++ src/components/ReportDetails.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileExport } from '@fortawesome/free-solid-svg-icons';
+
+const AdminReportCard = () => {
+  return (
+    <div className="border border-gray-300 rounded-lg p-4 mb-4 text-sm font-normal text-primary relative">
+      <div className="absolute top-0 right-0 mt-2 mr-2">
+        <FontAwesomeIcon icon={faFileExport} className="text-gray-400 text-lg" />
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="col-span-2 sm:col-span-1 mb-4">
+          <p className="text-primary text-base font-medium mb-2">Mode</p>
+          <p>Road</p>
+        </div>
+        <div className="col-span-2 sm:col-span-1 mb-4">
+          <p className="text-primary text-base font-medium mb-2">Status</p>
+          <p>Delivered</p>
+        </div>
+        <div className="col-span-2 sm:col-span-1 mb-4">
+          <p className="text-primary text-base font-medium mb-2">Customer PO</p>
+          <p>12345</p>
+        </div>
+        <div className="col-span-2 sm:col-span-1 mb-4">
+          <p className="text-primary text-base font-medium mb-2">FreightBroker PO</p>
+          <p>67890</p>
+        </div>
+        <div className="col-span-2 sm:col-span-1 mb-4">
+          <p className="text-primary text-base font-medium mb-2">Schedule Delivery Date</p>
+          <p>06/30/2024</p>
+        </div>
+        <div className="col-span-2 sm:col-span-1 mb-4">
+          <p className="text-primary text-base font-medium mb-2">Origin City</p>
+          <p>New York</p>
+        </div>
+        <div className="col-span-2 sm:col-span-1 mb-4">
+          <p className="text-primary text-base font-medium mb-2">Origin State</p>
+          <p>NY</p>
+        </div>
+        <div className="col-span-2 sm:col-span-1 mb-4">
+          <p className="text-primary text-base font-medium mb-2">Destination City</p>
+          <p>Los Angeles</p>
+        </div>
+        <div className="col-span-2 sm:col-span-1 mb-4">
+          <p className="text-primary text-base font-medium mb-2">Scheduled Pickup Date</p>
+          <p>06/28/2024</p>
+        </div>
+        <div className="col-span-2 sm:col-span-1 mb-4">
+          <p className="text-primary text-base font-medium mb-2">Destination</p>
+          <p>CA</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminReportCard;
--- src/pages/reportDetails.tsx
+++ src/pages/reportDetails.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
+import { faFileExport } from "@fortawesome/free-solid-svg-icons";
+
 
 const ReportDetails: React.FC = () => {
   return (
@@ -9,7 +11,9 @@ const ReportDetails: React.FC = () => {
       <Navbar />
 
       <div className="border border-gray-300 rounded-lg p-4 mb-4 text-sm font-normal px-20">
-        <h2 className="text-2xl text-gray-500 font-medium mb-4 text-center mt-5">REPORTING</h2>
+        <h2 className="text-2xl text-gray-500 font-medium mb-4 text-center mt-5">
+          REPORTING
+        </h2>
         <div className="flex flex-col sm:flex-row mb-4">
           <div className="w-full sm:w-1/2 mb-4 sm:mb-0 sm:pr-2">
             <label
@@ -35,7 +39,7 @@ const ReportDetails: React.FC = () => {
                   />
                 </div>
               </div>
-                     
+
               {/* End Date Input */}
               <div className="relative ml-2">
                 <input
@@ -51,6 +55,72 @@ const ReportDetails: React.FC = () => {
                   />
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className="border border-gray-300 rounded-lg p-4 mb-4 text-sm font-normal text-primary relative">
+          <div className="absolute top-0 right-0 mt-2 mr-2">
+            <FontAwesomeIcon
+              icon={faFileExport}
+              className="text-gray-400 text-lg"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="col-span-2 sm:col-span-1 mb-4">
+              <p className="text-primary text-base font-medium mb-2">Mode</p>
+              <p>Road</p>
+            </div>
+            <div className="col-span-2 sm:col-span-1 mb-4">
+              <p className="text-primary text-base font-medium mb-2">Status</p>
+              <p>Delivered</p>
+            </div>
+            <div className="col-span-2 sm:col-span-1 mb-4">
+              <p className="text-primary text-base font-medium mb-2">
+                Customer PO
+              </p>
+              <p>12345</p>
+            </div>
+            <div className="col-span-2 sm:col-span-1 mb-4">
+              <p className="text-primary text-base font-medium mb-2">
+                FreightBroker PO
+              </p>
+              <p>67890</p>
+            </div>
+            <div className="col-span-2 sm:col-span-1 mb-4">
+              <p className="text-primary text-base font-medium mb-2">
+                Schedule Delivery Date
+              </p>
+              <p>06/30/2024</p>
+            </div>
+            <div className="col-span-2 sm:col-span-1 mb-4">
+              <p className="text-primary text-base font-medium mb-2">
+                Origin City
+              </p>
+              <p>New York</p>
+            </div>
+            <div className="col-span-2 sm:col-span-1 mb-4">
+              <p className="text-primary text-base font-medium mb-2">
+                Origin State
+              </p>
+              <p>NY</p>
+            </div>
+            <div className="col-span-2 sm:col-span-1 mb-4">
+              <p className="text-primary text-base font-medium mb-2">
+                Destination City
+              </p>
+              <p>Los Angeles</p>
+            </div>
+            <div className="col-span-2 sm:col-span-1 mb-4">
+              <p className="text-primary text-base font-medium mb-2">
+                Scheduled Pickup Date
+              </p>
+              <p>06/28/2024</p>
+            </div>
+            <div className="col-span-2 sm:col-span-1 mb-4">
+              <p className="text-primary text-base font-medium mb-2">
+                Destination
+              </p>
+              <p>CA</p>
             </div>
           </div>
         </div>
