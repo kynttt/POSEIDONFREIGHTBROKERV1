ec4d305477bbe6f4443066230f9543778ae124f9
Aldren Kent  Cirunay <131524402+kynttt@users.noreply.github.com>
2024-07-02 21:22:58 -0700

    update

--- src/components/TrustedAmazon.tsx
+++ src/components/TrustedAmazon.tsx
@@ -2,7 +2,7 @@
 
         const TrustedAmazon: React.FC = () => {
             return (
-                <div className="flex flex-col items-center justify-center min-full-screen py-12 px-4 md:px-8 lg:px-16 xl:px-24">
+                <div className="bg-white flex flex-col items-center justify-center min-full-screen py-12 px-4 md:px-8 lg:px-16 xl:px-24">
                     <div className="text-center">
                         <h1 className="text-3xl md:text-3xl text-primary font-normal mb-4">Trusted by Amazon</h1>
                         <p className="text-lg md:text-xl lg:text-xl text-primary font-normal mb-6">We are a reliable Amazon Linehaul Contractor.</p>
