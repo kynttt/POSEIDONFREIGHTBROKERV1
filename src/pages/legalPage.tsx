7efc676c89b518c1eb84e9a8c15698575b6ee72f
KennethKnows <abenojaken@gmail.com>
2024-07-04 14:36:40 +0800

    legal page update 070424

--- src/pages/legalPage.tsx
+++ src/pages/legalPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFolder, faSearch, faUpload, faPlus, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { faFolderOpen, faSearch, faArrowUpFromBracket, faPlus, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 const LegalPage = () => {
   const folders = [
@@ -25,51 +25,49 @@ const LegalPage = () => {
     <nav>
     <Navbar isAuthenticated={false} />
     
-    <div className="p-8 min-full">
-      <div className="flex justify-between items-start mb-4 px-12">
-  <div className="mb-8">
-    <h1 className="text-2xl font-semibold text-blue-600">Hello Legal Team,</h1>
-    <h2 className="text-xl text-blue-600">Good Morning</h2>
-  </div>
-
-  <div className="relative">
-    <input
-      type="text"
-      placeholder="Search File"
-      className="border rounded py-2 px-3 pr-10 focus:outline-none focus:shadow-outline"
-    />
-    <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-      <FontAwesomeIcon icon={faSearch} className="text-gray-500" />
-    </div>
-  </div>
-</div>
-
-<div className="flex justify-end items-start mt-4 px-12">
-  <div className="flex items-end gap-4">
-    <button className="text-blue-600 flex items-end gap-2 border border-blue-600 rounded px-3 py-1">
-      <FontAwesomeIcon icon={faUpload} />
-      Upload Files
-    </button>
-    <button className="text-blue-600 flex items-end gap-2 border border-blue-600 rounded px-3 py-1">
-      <FontAwesomeIcon icon={faPlus} />
-      Create New
-    </button>
-  </div>
-</div>
-
-
-      
-<div className="mb-8 px-24">
-  <h3 className="text-lg font-semibold text-blue-600 mb-4">All Folders</h3>
-  <div className="flex gap-4 overflow-x-auto gap-x-12">
-    {folders.map((folder) => (
-      <div key={folder} className="flex-shrink-0 border border-blue-600 rounded-lg p-4 text-center text-blue-600" style={{ minWidth: '150px', maxWidth: '150px', minHeight: '100px' }}>
-        <FontAwesomeIcon icon={faFolder} className="text-3xl mb-2" />
-        <p>{folder}</p>
+    <div className="p-4 sm:p-6 md:p-8 min-full">
+    <div className="flex flex-col md:flex-row justify-between items-start mt-4 mb-4 px-4 sm:px-6 md:px-12">
+      <div className="mb-4 md:mb-8">
+        <h2 className="text-sm font-light text-secondary">Hello Legal Team,</h2>
+        <h2 className="text-xl font-medium text-secondary">Good Morning</h2>
       </div>
-    ))}
-  </div>
-</div>
+
+      <div className="relative w-full md:w-auto mt-4 md:mt-0">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <FontAwesomeIcon icon={faSearch} className="text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search File"
+          className="w-full md:w-64 text-sm font-normal border rounded py-3 px-4 pl-10 focus:outline-none focus:shadow-outline"
+        />
+      </div>
+    </div>
+
+    <div className="flex justify-end items-start mt-4 px-4 sm:px-6 md:px-12 mb-6">
+      <div className="flex items-end gap-4">
+        <button className="text-sm font-normal text-gray-400 flex items-end gap-2 border border-gray-400 rounded px-3 py-2">
+          <FontAwesomeIcon icon={faArrowUpFromBracket} />
+          Upload Files
+        </button>
+        <button className="text-sm text-gray-400 flex items-end gap-2 border border-gray-400 rounded px-2 py-2 font-normal">
+          <FontAwesomeIcon icon={faPlus} />
+          Create New
+        </button>
+      </div>
+    </div>
+
+    <div className="mb-8 px-4 sm:px-6 md:px-32">
+      <h3 className="text-xl font-medium text-secondary mb-8">All Folders</h3>
+      <div className="flex gap-4 overflow-x-auto gap-x-12">
+        {folders.map((folder) => (
+          <div key={folder} className="text-sm flex-shrink-0 border-2 border-secondary rounded-lg p-4 text-center text-primary" style={{ minWidth: '150px', maxWidth: '150px', minHeight: '100px' }}>
+            <FontAwesomeIcon icon={faFolderOpen} className="mt-1 text-3xl mb-2 text-gray-500 hover:text-secondary" />
+            <p>{folder}</p>
+          </div>
+        ))}
+      </div>
+    </div>
 
 
       <div className="mb-8">
