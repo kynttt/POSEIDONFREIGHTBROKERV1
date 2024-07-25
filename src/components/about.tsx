dd76155fe2b530f164f52cac5545b7ccdb9064dd
Aldren Kent  Cirunay <131524402+kynttt@users.noreply.github.com>
2024-07-24 19:59:12 -0700

    update

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,15 +3,15 @@ import AboutImage from "../assets/img/aboutus.png";
 
 const AboutUs: React.FC = () => {
   return (
-    <div className="bg-[#eaeefa] flex flex-col lg:flex-row items-center lg:items-start px-4 md:px-8 lg:px-20 xl:px-32 gap-8 lg:gap-20">
+    <div className="bg-[#eaeefa] flex flex-col lg:flex-row items-center lg:items-center px-4 md:px-8 lg:px-20 xl:px-32  ">
       {/* Image Section */}
       
-      <div className="w-full lg:w-1/2 flex justify-center items-center mb-10 lg:order-last">
-        <img src={AboutImage} alt="About Us" className="w-full h-auto lg:max-h-full object-cover object-center" />
+      <div className="w-full lg:w-2/3 flex justify-end items-center mb-10 lg:order-last">
+        <img src={AboutImage} alt="About Us" className="lg:w-4/5 h-auto lg:max-h-full object-cover object-center  " />
       </div>
 
       {/* Content Section */}
-      <div className="w-full lg:w-1/2 flex flex-col justify-center text-[#252F70] max-w-prose lg:ml-2px lg:mt-40 mb-5">
+      <div className="w-full lg:w-1/3 flex flex-col justify-center text-[#252F70] max-w-prose lg:ml-2px lg:mt-16 mb-5">
         <p className="text-xl font-medium mb-4 lg:mb-8">About Us</p>
         <h1 className="text-3xl text-gray-600 mb-4 lg:mb-8 text-primary">Transport and Logistics</h1>
         <p className="text-gray-500 text-md font-normal leading-relaxed text-justify">
