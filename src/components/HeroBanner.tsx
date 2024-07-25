3e4b4314a0e19db814f19c62b4a776dfa82f8c34
Aldren Kent  Cirunay <131524402+kynttt@users.noreply.github.com>
2024-07-24 18:37:17 -0700

    updated

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -15,13 +15,13 @@ const HeroBanner: React.FC = () => {
       className="relative flex flex-col md:flex-row justify-between items-start px-6 md:px-12 lg:px-24 xl:px-32 lg:w-full lg:h-5/6 md:h-5/6 md:h-1/2 sm:h-1/2"
       style={{
         background:
-          "linear-gradient(-180deg, #7783D2 13%, #F0F0F0 83%)",
+          "linear-gradient(-180deg, #7783D2 1%, #f4f4f4 60%)",
       }}
     >
       {/* Parent container for both text content and image */}
-      <div className="flex flex-col md:flex-row items-start justify-between w-full lg:h-full mt-5 lg:mt-7">
+      <div className="flex flex-col md:flex-row items-start justify-between w-full lg:h-full my-5 lg:py-14 lg:px-28">
         {/* Image Section (mobile first) */}
-        <div className="md:hidden w-full relative mb-8 ml-2">
+        <div className="md:hidden w-full relative mb-8 ml-2 lg:w-1/2">
           <img
             src={heroBanner}
             alt="Hero Banner"
@@ -31,18 +31,18 @@ const HeroBanner: React.FC = () => {
         </div>
 
         {/* Content Section */}
-        <div className="flex-1 md:px-10 lg:px-10 md:mt-12 lg:mt-32 sm:mt-8 z-10">
+        <div className="flex-1   md:mt-12 lg:mt-32 sm:mt-8 z-10 lg:mr-6">
           <h1
-            className="text-3xl md:text-5xl xl:text-7xl font-medium text-white"
+            className="text-3xl md:text-5xl xl:text-6xl font-medium text-white"
             style={{ textShadow: "0px 4px 6px rgba(0, 0, 2, 0.3)" }}
           >
             TRANSPORT
           </h1>
 
           <h2
-            className="text-2xl md:text-4xl xl:text-7xl font-black mt-2 mb-5 relative"
+            className="text-2xl md:text-4xl xl:text-6xl font-black mt-2 mb-5 relative"
           >
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-900 to-blue-600 relative z-10">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-900 to-blue-600 relative z-10 ">
               LOGISTICS
             </span>
             <span
@@ -69,7 +69,7 @@ const HeroBanner: React.FC = () => {
         </div>
 
         {/* Image Section (desktop) */}
-        <div className="hidden md:block md:w-1/2 lg:w-2/5 relative">
+        <div className="hidden md:block md:w-1/2 lg:w-1/2  relative">
           <img
             src={heroBanner}
             alt="Hero Banner"
