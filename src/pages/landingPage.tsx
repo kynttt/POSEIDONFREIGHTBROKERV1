48fa32159f337e2ad7f0779c834c53438824c31e
Mosh Ontong <moshOntong@gmail.com>
2024-08-05 09:15:08 +0800

    feat: added mantine dependency and refactor Hero

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -1,45 +1,46 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import '../index.css';  // Ensure this file includes the @import for the Lexend font
-import HeroBanner from '../components/HeroBanner';
+import React from "react";
+import Navbar from "../components/Navbar";
+import "../index.css"; // Ensure this file includes the @import for the Lexend font
+import HeroBanner from "../components/HeroBanner";
 // import Carousel from '../components/Carousel';
-import FreightQuote from '../components/FreightQuote';
-import AboutUs from '../components/about';
+import FreightQuote from "../components/FreightQuote";
+import AboutUs from "../components/about";
 // import SuccessPage from '../components/SuccessPage';
-import FAQsPage from '../components/Faqs';
-import FeaturesSection from '../components/Features';
-import Footer from '../components/Footer';
+import FAQsPage from "../components/Faqs";
+import FeaturesSection from "../components/Features";
+import Footer from "../components/Footer";
 // import ServicesCardList from '../components/ServicesCardList';
-import SeamlessFreightServices from '../components/SeamlessFreightServices';
-import ContactForm from '../components/ContactUs';
-import TrustedAmazon from '../components/TrustedAmazon';
-import { useAuthStore } from '../state/useAuthStore';
-
-
+import SeamlessFreightServices from "../components/SeamlessFreightServices";
+import ContactForm from "../components/ContactUs";
+import TrustedAmazon from "../components/TrustedAmazon";
+import { useAuthStore } from "../state/useAuthStore";
+import { Stack } from "@mantine/core";
 
 const LandingPage: React.FC = () => {
-  const {  isAuthenticated } = useAuthStore();
+  const { isAuthenticated } = useAuthStore();
 
   return (
     <div className="min-h-screen">
       <header>
-      <Navbar isAuthenticated={isAuthenticated} />
+        <Navbar isAuthenticated={isAuthenticated} />
       </header>
-      <main>
-      <HeroBanner />
-      <SeamlessFreightServices />
-      {/* <ServicesCardList/> */}
-      {/* <Carousel /> */}
-      <FreightQuote/>
-      <FeaturesSection />
-      <AboutUs />
-      <FAQsPage />
-      <TrustedAmazon />
-      <ContactForm />
-      <Footer/>
-      {/* <SuccessPage /> */}
-        
-      </main>
+      <Stack>
+        <HeroBanner />
+        <SeamlessFreightServices />
+        {/* <ServicesCardList/> */}
+        {/* <Carousel /> */}
+        <FreightQuote />
+        <FeaturesSection />
+        <AboutUs />
+        <FAQsPage />
+        <TrustedAmazon />
+        <ContactForm />
+
+        {/* <SuccessPage /> */}
+      </Stack>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
