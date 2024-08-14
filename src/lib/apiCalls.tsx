c53afbeab77be62f8444377d51158a5cad37221a
Muslimin Ontong <moshOntong@gmail.com>
2024-08-14 14:10:19 +0800

    Update apiCalls.tsx

--- src/lib/apiCalls.tsx
+++ src/lib/apiCalls.tsx
@@ -1,11 +1,5 @@
 import axios from "axios";
-import {
-  Booking,
-  BookingCallback,
-  Invoice,
-  Quote,
-  RegisterFormData,
-} from "../utils/types";
+import { Booking, Invoice, Quote, RegisterFormData } from "../utils/types";
 
 // Set the base URL for the API
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
