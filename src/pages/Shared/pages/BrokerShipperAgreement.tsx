f85dbdde738d727ff70a37893fd4f32193bb537e
Aldren Kent  Cirunay <131524402+kynttt@users.noreply.github.com>
2024-08-20 14:00:19 +0700

    update

--- src/pages/Shared/pages/BrokerShipperAgreement.tsx
+++ src/pages/Shared/pages/BrokerShipperAgreement.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const BrokerShipperAgreement: React.FC = () => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const [agreed, setAgreed] = useState(false);
 
     const handleCheckboxChange = () => {
