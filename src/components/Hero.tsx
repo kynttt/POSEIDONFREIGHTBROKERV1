b05c8b8809583b23eed4a94618b7c55f7829f52f
Aldren Kent  Cirunay <131524402+kynttt@users.noreply.github.com>
2024-08-29 13:35:04 +0700

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,13 +8,13 @@ const HeroBanner: React.FC = () => {
   const trustRating = 95; // Example trust rating value
 
   return (
-    <div className="relative w-full h-screen overflow-hidden">
+    <div className="relative w-full h-screen overflow-hidden h-[90vh]">
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{ backgroundImage: `url(${heroTruck})` }}
       />
 
-      <div className="absolute inset-0 grid grid-cols-1 lg:grid-cols-3 gap-4 text-white p-8 z-10">
+      <div className="absolute inset-0 grid grid-cols-1 lg:grid-cols-3 gap-4 text-white p-8 z-10 ">
         <div className="hidden lg:flex lg:mt-28 w-2/5 h-1/3 mx-auto text-white flex items-center justify-center">
           <div className="p-4 md:p-6 text-center bg-accentBg rounded-2xl shadow-2xl">
             <h2 className="text-xl md:text-2xl font-semibold mb-2 md:mb-4">
@@ -56,7 +56,7 @@ const HeroBanner: React.FC = () => {
         </div>
       </div>
 
-      <div className="shadow-2xl w-11/12 sm:w-4/5 md:w-3/5 lg:w-3/5 border border-purple-50 border-opacity-5 rounded-2xl absolute bottom-20 sm:bottom-28 md:bottom-44 left-1/2 transform -translate-x-1/2 py-8 sm:py-10 md:py-12 px-6 sm:px-12 lg:px-20 text-white z-20 bg-gray-900/50 backdrop-blur-sm">
+      <div className="shadow-2xl w-11/12 sm:w-4/5 md:w-3/5 lg:w-3/5 border border-purple-50 border-opacity-5 rounded-2xl absolute bottom-20 sm:bottom-28 md:bottom-24 left-1/2 transform -translate-x-1/2 py-8 sm:py-10 md:py-12 px-6 sm:px-12 lg:px-20 text-white z-20 bg-gray-900/50 backdrop-blur-sm">
   <FreightQuote2/>
 </div>
 
