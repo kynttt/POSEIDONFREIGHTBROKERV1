0f61f4e0b753472d0a303fd1ce2f5cf949c2f4f9
Aldren Kent  Cirunay <131524402+kynttt@users.noreply.github.com>
2024-09-12 10:24:57 +0700

    minor ui update

--- src/pages/Shared/pages/forgotPassword.tsx
+++ src/pages/Shared/pages/forgotPassword.tsx
@@ -3,6 +3,8 @@ import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { notifications } from "@mantine/notifications";
 import axiosInstance from "../../../lib/axiosInstance";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamation } from "@fortawesome/free-solid-svg-icons";
 
 interface ForgotPasswordData {
   email: string;
@@ -27,7 +29,7 @@ const ForgotPasswordPage: React.FC = () => {
         title: "Error",
         message: `${
           (error.response?.data as { message: string }).message ||
-          "Something went wrong"
+          "User with this email does not exist"
         }`,
         color: "red",
       });
@@ -41,18 +43,19 @@ const ForgotPasswordPage: React.FC = () => {
 
   return (
     <div className="flex h-screen justify-center items-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
-        <h1 className="text-2xl font-bold text-center text-gray-800 mb-4">
-          Forgot Password
-        </h1>
-        <p className="text-center text-gray-600 mb-6">
+      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
+      <h2 className="mb-1 flex items-center text-2xl font-semibold text-start text-primary">
+          <FontAwesomeIcon icon={faExclamation} className="bg-grey w-4 h-4 rounded-full p-2 mr-2 text-primary" />
+          Forget Password
+        </h2>
+        <p className="text-sm font-normal text-gray-500 mb-8">
           Enter your email address and we will send you a reset link.
         </p>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label
               htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
+              className="block mb-2 text-sm font-medium text-gray-600"
             >
               Email
             </label>
@@ -63,23 +66,23 @@ const ForgotPasswordPage: React.FC = () => {
               onChange={(e) => setEmail(e.target.value)}
               placeholder="Enter your email"
               required
-              className="w-full px-4 py-2 mt-1 border rounded-md focus:ring-blue-500 focus:border-blue-500 bg-gray-50"
+              className="w-full px-4 py-2 text-sm bg-light-grey rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 font-normal"
             />
           </div>
           <button
             type="submit"
             disabled={mutation.isPending}
-            className={`w-full py-2 px-4 text-white font-semibold rounded-md ${
+            className={`w-full border-t px-4 py-2 font-medium text-white bg-primary rounded-md hover:bg-secondary focus:outline-none focus:bg-secondary ${
               mutation.isPending
                 ? "bg-gray-400 cursor-not-allowed"
-                : "bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                : "bg-primary hover:bg-secondary  focus:outline-none focus:ring-2 focus:ring-blue-500"
             }`}
           >
             {mutation.isPending ? "Sending..." : "Send Reset Link"}
           </button>
         </form>
         <div className="text-center text-gray-500 mt-4">
-          <a href="/login" className="text-blue-600 hover:underline">
+          <a href="/login" className="text-blue-600 hover:underline text-sm font-medium">
             Back to login
           </a>
         </div>
