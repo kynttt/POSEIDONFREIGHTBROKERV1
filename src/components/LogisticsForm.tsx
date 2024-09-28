166226e3c884f088c86391a3b351762139d93ae5
Aldren Kent Cirunay <aldrencirunay@gmail.com>
2024-09-28 22:30:25 +0800

    update ui

--- src/components/LogisticsForm.tsx
+++ src/components/LogisticsForm.tsx
@@ -7,7 +7,7 @@ const LogisticsForm: React.FC = () => {
   return (
     <div className="relative w-full h-3/4">
       {/* Dark Overlay for md to xs screens */}
-      <div className="absolute inset-0 bg-black opacity-50 md:opacity-60 lg:opacity-50" />
+      <div className="absolute inset-0 bg-black opacity-50 md:opacity-60 lg:opacity-20" />
       
       {/* Background Section */}
       <div
@@ -24,11 +24,12 @@ const LogisticsForm: React.FC = () => {
         {/* Right Section */}
         <div className="lg:w-1/2 w-full bg-transparent p-8 lg:p-16 text-white relative">
           <div className="lg:w-5/7 mx-auto">
-            <h2 className="text-2xl lg:text-3xl font-bold mb-4 text-center">
-              Transform Your Logistics Today—
+            <h2 className="text-2xl lg:text-3xl font-bold mb-8 md:text-center sm:text-left">
+            Get Quotation, pay and book 
+            your freight transportation
               <h2 className="text-yellow-500">24 Hours Operating support services</h2>
             </h2>
-            <p className="text-sm lg:text-base mb-8 font-normal text-justify lg:px-0 sm:px-24">
+            <p className="text-sm lg:text-base mb-8 font-normal lg:w-3/4 mx-auto text-justify lg:px-0 sm:px-24">
               Through Third-Party Logistics (3PL) services, businesses can achieve the flexibility and expertise required to streamline operations and lower expenses, allowing you to prioritize your core business as we oversee logistics, including expedited shipping and full truck loads.
             </p>
           </div>
@@ -38,12 +39,12 @@ const LogisticsForm: React.FC = () => {
             {/* Origin Location */}
             <div className="mb-4">
               <label htmlFor="origin" className="block text-md font-medium text-rblue">
-                Origin Location
+              Choose pick-up destination
               </label>
               <input
                 type="text"
                 id="origin"
-                className="mt-1 px-4 py-3 block w-full rounded-lg border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 border border-2 placeholder-font-thin"
+                className="bg-grey mt-1 px-4 py-3 block w-full rounded-lg  shadow-sm focus:border-blue-500 focus:ring-blue-500  placeholder-font-thin"
                 placeholder="Enter Origin Location"
                 style={{ fontWeight: '300' }} // Make the placeholder font thin
               />
@@ -52,12 +53,12 @@ const LogisticsForm: React.FC = () => {
             {/* Destination Location */}
             <div className="mb-4">
               <label htmlFor="destination" className="block text-md text-rblue font-medium">
-                Destination Location
+              Choose drop-off location
               </label>
               <input
                 type="text"
                 id="destination"
-                className="mt-1 px-4 py-3 block w-full rounded-lg border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 border border-2 placeholder-font-thin"
+                className="bg-grey mt-1 px-4 py-3 block w-full rounded-lg  shadow-sm focus:border-blue-500 focus:ring-blue-500  placeholder-font-thin"
                 style={{ fontWeight: '300' }}
                 placeholder="Enter Destination Location"
               />
@@ -66,11 +67,11 @@ const LogisticsForm: React.FC = () => {
             {/* Trailer Type */}
             <div className="mb-6">
               <label htmlFor="trailerType" className="block text-md text-rblue font-medium">
-                Trailer Type
+              Choose your trailer type
               </label>
               <select
                 id="trailerType"
-                className="mt-1 px-4 py-3 block w-full rounded-lg border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 border border-2"
+                className="bg-grey mt-1 px-4 py-3 block w-full rounded-lg  shadow-sm focus:border-blue-500 focus:ring-blue-500 "
                 style={{ fontWeight: '300' }}
               >
                 <option>Select Trailer Type</option>
@@ -85,7 +86,7 @@ const LogisticsForm: React.FC = () => {
             <div className="text-center">
                 <button
                 type="submit"
-                className="md:w-1/2 bg-yellow-500 text-blue-900 py-3 px-4 rounded-md shadow-sm font-bold hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+                className="md:w-1/2 w-full bg-yellow-500 text-blue-900 py-3 px-4 rounded-md shadow-sm font-bold hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
                 onClick={() => navigate("/requests")}
                 >
                 Request Quote
